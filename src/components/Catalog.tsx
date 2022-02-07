ea9250fc3f2e4c75bab0c5254c5f1fd1adbe3772
Renan Veronez Drechsler <renanvz@outlook.com>
2022-02-07 19:51:55 -0300

    Server JSON

--- src/components/Catalog.tsx
+++ src/components/Catalog.tsx
@@ -1,12 +1,31 @@
-import { useSelector } from "react-redux";
+import { useEffect, useState } from "react";
+import { IProduct } from "../store/modules/cart/types";
+import api from "../services/api";
 
 const Catalog: React.FC = () => {
-  const catalog = useSelector(state => state);
+  const [catalog, setCatalog] = useState<IProduct[]>([]);
+
+  useEffect(() => {
+    api.get('products').then(response => {
+      setCatalog(response.data);
+    })
+  }, [])
 
   console.log(catalog)
 
   return (
-    <h1>Catalog</h1>
+    <main>
+      <h1>Catalog</h1>
+
+      {catalog.map(product => (
+        <article key={product.id}>
+          <strong>{product.title}</strong> {" - "}
+          <span>{product.price}</span> {"  "}
+
+          <button type="button">Comprar</button>
+        </article>
+      ))}
+    </main>
   )
 }
 
