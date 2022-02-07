88558389391ad8dc58c3570db328983e3fe0cd0d
Renan Veronez Drechsler <renanvz@outlook.com>
2022-02-07 20:42:24 -0300

    Alterando Estado

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { Provider } from 'react-redux'
 import Catalog from './components/Catalog';
-import store from './store';
+import Cart from './components/Cart';
 
+import store from './store';
 
 function App() {
   return (
     <Provider store={store}>
       <Catalog />
+      <Cart />
     </Provider>
   );
 }
---
+++ src/components/Cart.tsx
@@ -0,0 +1,25 @@
+import { useSelector } from "react-redux"
+
+const Cart: React.FC = () => {
+  const state = useSelector(state => state)
+
+  console.log(state);
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Produto</th>
+          <th>Preço</th>
+          <th>Quantidade</th>
+          <th>Subtotal</th>
+        </tr>
+      </thead>
+      <tbody>
+
+      </tbody>
+    </table>
+  )
+}
+
+export default Cart;
